aad2e0c8cfe338fabe0c52f4e7ac3f074dc59b1e
vamsikrish1 <vamsi010@outlook.com>
2021-09-12 12:37:02 +0530

    initial setup

---
+++ src/test/java/com/example/elearning/learning/LearningApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.example.elearning.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
